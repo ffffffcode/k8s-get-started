75f0b96ca282c2cf1dadd5625a0e8eb1a44ed71a
Aaron <a1233456>
2021-07-20 15:34:17 +0800

    first commit

---
+++ src/main/java/com/boomsj/aaron/k8sgetstarted/K8sGetStartedApplication.java
@@ -0,0 +1,13 @@
+package com.boomsj.aaron.k8sgetstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sGetStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sGetStartedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boomsj/aaron/k8sgetstarted/K8sGetStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boomsj.aaron.k8sgetstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sGetStartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
